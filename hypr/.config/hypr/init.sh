d7d7ffb5a8411eace45b37abd2acce8d827f8456
Kshitij Sharma <kshitij.sharma.cse22@itbhu.ac.in>
2025-06-12 19:27:01 +0530

    Added wezterm to init and fileBrowser menu for rofi

--- hypr/.config/hypr/init.sh
+++ hypr/.config/hypr/init.sh
@@ -2,7 +2,7 @@
 
 hyprctl dispatch exec '[workspace 1 silent] code'
 hyprctl dispatch exec '[workspace 2 silent] zen-browser'
-hyprctl dispatch exec '[workspace 3 silent] kitty'
+hyprctl dispatch exec '[workspace 3 silent] wezterm'
 hyprctl dispatch exec '[workspace 4 silent] spotify'
 hyprctl dispatch exec '[workspace 5 silent] obsidian'
 
