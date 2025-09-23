02a7114b43f4f20b1b8abe11ba966df245bbf978
Kshitij Sharma <kshitijs1202@gmail.com>
2025-09-23 07:22:16 +0530

    add script to sync with rclone

---
+++ scripts/.local/bin/rclone-bisync.sh
@@ -0,0 +1,4 @@
+rclone bisync --verbose --progress onedrive:Pictures/icons ~/pic/icons
+rclone bisync --verbose --progress onedrive:Pictures/wallpapers-pc-gif ~/pic/wallpapers-pc-gif
+rclone bisync --verbose --progress onedrive:Pictures/wallpapers-pc ~/pic/wallpapers-pc
+rclone bisync --verbose --progress onedrive:Documents/conart ~/doc/conart
