d9d4e86324b7d1ace571ef38bd06c42799b09be2
Kshitij Sharma <kshitijs1202@gmail.com>
2025-10-11 21:21:01 +0530

    update dir names

--- scripts/.local/bin/rclone-bisync.sh
+++ scripts/.local/bin/rclone-bisync.sh
@@ -1,4 +1,6 @@
-rclone bisync --verbose --progress onedrive:Pictures/icons ~/pic/icons
-rclone bisync --verbose --progress onedrive:Pictures/wallpapers-pc-gif ~/pic/wallpapers-pc-gif
-rclone bisync --verbose --progress onedrive:Pictures/wallpapers-pc ~/pic/wallpapers-pc
-rclone bisync --verbose --progress onedrive:Documents/conart ~/doc/conart
+rclone bisync --verbose --progress onedrive:Pictures/icons ~/Pictures/icons
+rclone bisync --verbose --progress onedrive:Pictures/wallpapers-pc-gif ~/Pictures/wallpapers-pc-gif
+rclone bisync --verbose --progress onedrive:Pictures/wallpapers-pc ~/Pictures/wallpapers-pc
+rclone bisync --verbose --progress onedrive:Pictures/screen-linux ~/Pictures/screen
+rclone bisync --verbose --progress onedrive:Documents/conart ~/Documents/conart
+rclone bisync --verbose --progress onedrive:Documents/Cryptomator ~/Documents/crypt
