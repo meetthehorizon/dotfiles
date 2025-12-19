e7b0208694852644a9ed2b124e2d4efacfc15b5a
Kshitij Sharma <kshitijs1202@gmail.com>
2025-12-20 02:23:10 +0530

    flat design for focus!

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,7 +14,7 @@ return {
 
 	keys = {
 		{
-			key = "F11", -- or any key you like
+			key = "F11",
 			mods = "NONE",
 			action = wezterm.action.ToggleFullScreen,
 		},
@@ -22,7 +22,22 @@ return {
 
 	default_prog = { "/usr/bin/fish", "-l" },
 	font_size = 11.0,
-	window_background_opacity = 0.40,
 	enable_tab_bar = false,
 	window_decorations = "RESIZE",
+
+	-- Catppuccin Mocha colors
+	color_scheme = "Catppuccin Mocha",
+
+	-- Solid background (no transparency)
+	window_background_opacity = 1.0,
+
+	colors = {
+		background = "#1e1e2e",
+		foreground = "#cdd6f4",
+		cursor_bg = "#f5e0dc",
+		cursor_fg = "#1e1e2e",
+		cursor_border = "#f5e0dc",
+		selection_bg = "#45475a",
+		selection_fg = "#cdd6f4",
+	},
 }
