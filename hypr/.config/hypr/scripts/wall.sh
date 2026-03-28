193751ff3a773760d66770a2eaa4c7f44be92d2e
Kshitij Sharma <kshitijs1202@gmail.com>
2026-03-28 11:48:42 +0530

    update variety interaction

--- hypr/.config/hypr/scripts/wall.sh
+++ hypr/.config/hypr/scripts/wall.sh
@@ -6,7 +6,6 @@ CACHE_WALL="$HOME/.cache/wallpaper"
 variety -n
 
 # 2. Short sleep to ensure Variety has finished the file swap
-sleep 0.5
 
 # 3. Get the path, stripping out the "Already running" message
 # We use tail -n 1 to grab only the last line (the path)
@@ -14,9 +13,6 @@ WALL=$(variety --current | tail -n 1)
 
 # 4. Validate and Link
 if [ -f "$WALL" ]; then
-    # Set with your preferred swww settings
-    swww img "$WALL" --transition-type center --transition-duration 1 --transition-fps 120
-
     # Update your cache symlink
     ln -sf "$WALL" "$CACHE_WALL"
     
