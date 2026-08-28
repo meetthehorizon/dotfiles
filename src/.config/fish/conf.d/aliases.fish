7e4707daba91231c6c3d7604df3b1df51181ff16
Kshitij Sharma <kshitij.dev@proton.me>
2026-08-28 11:23:13 +0530

    refactor: fish abbr and remove systemctl abbr

--- src/.config/fish/conf.d/aliases.fish
+++ src/.config/fish/conf.d/aliases.fish
@@ -5,25 +5,22 @@ if status is-interactive
 
     # Abbreviations
     abbr -a v "nvim"
-    
+
     # Git
-    abbr -a g "git"
-    abbr -a ga "git add"
-    abbr -a gc "git commit"
-    abbr -a gp "git push"
-    abbr -a gs "git status"
-    abbr -a gb "git branch"
+    abbr -a g   "git"
+    abbr -a ga  "git add"
+    abbr -a gc  "git commit"
+    abbr -a gp  "git push"
+    abbr -a gs  "git status"
+    abbr -a gb  "git branch"
+    abbr -a gr  "git rebase"
     abbr -a gpl "git pull"
     abbr -a gfk "git commit --amend --no-edit"
     abbr -a gco "git checkout"
-    abbr -a glo "git log --oneline"
+    abbr -a glo "git log --oneline -n "
     abbr -a grs "git restore --staged"
     abbr -a grh "git reset --hard"
-    abbr -a grb "git rebase"
-    abbr -a grbs "git rebase -i --autosquash --root"
-    abbr -a grbc "git rebase --continue"
+    abbr -a grs "git rebase -i --autosquash --root"
+    abbr -a grc "git rebase --continue"
 
-    # Systemd
-    abbr -a ss "systemctl status"
-    abbr -a sr "sudo systemctl restart"
 end
