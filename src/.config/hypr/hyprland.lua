6cf54a7467ef234015531edae701262c8a535b08
Kshitij Sharma <kshitij.dev@proton.me>
2026-08-28 21:48:03 +0530

    feat: add Thunar to `center-float` window rule

--- src/.config/hypr/hyprland.lua
+++ src/.config/hypr/hyprland.lua
@@ -370,7 +370,7 @@ hl.layer_rule({
 
 hl.window_rule({
 	name = "center-float",
-	match = { class = "xdg-desktop-portal-gtk|.*rog-control-center|.*seahorse.*|" },
+	match = { class = "xdg-desktop-portal-gtk|.*rog-control-center|.*seahorse.*|.*Thunar.*" },
 	float = true,
 	min_size = { 1000, 800 },
 	max_size = { 1000, 800 },
