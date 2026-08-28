6ed83eed48f17cf44559136f114b55255b6810dc
Kshitij Sharma <kshitij.dev@proton.me>
2026-08-28 19:16:42 +0530

    feat: made caps act as escape, nat scoll, and middle click

--- src/.config/hypr/hyprland.lua
+++ src/.config/hypr/hyprland.lua
@@ -204,17 +204,12 @@ hl.config({
 hl.config({
     input = {
         kb_layout  = "us",
-        kb_variant = "",
-        kb_model   = "",
-        kb_options = "",
-        kb_rules   = "",
-
+        kb_options = "caps:escape",
         follow_mouse = 1,
 
-        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
-
         touchpad = {
-            natural_scroll = false,
+            natural_scroll = true,
+            middle_button_emulation = true,
         },
     },
 })
